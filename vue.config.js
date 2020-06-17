ff5ad89780413f0c2e8ea17821fc88cf2aa9fc51
wan.yixin <962717593@qq.com>
2020-06-17 15:52:26 +0800

    '添加postcssautopre支持'

--- vue.config.js
+++ vue.config.js
@@ -19,17 +19,5 @@ module.exports = {
             preProcessor: 'less',
             patterns: [path.resolve(__dirname, './src/assets/styles/public.less')]
         }
-    },
-    css: {
-        loaderOptions: {
-            css: {
-                // 这里的选项会传递给 css-loader
-                importLoaders: 1
-            },
-            less: {
-                // 这里的选项会传递给 postcss-loader
-                importLoaders: 1
-            }
-        }
     }
 };
