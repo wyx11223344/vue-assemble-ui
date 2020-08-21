983aa330544ce80f5cbdd13c4629f6118bfa08be
wan.yixin <962717593@qq.com>
2020-08-21 14:45:04 +0800

    '立即触发'

--- src/components/select/selectTools.ts
+++ src/components/select/selectTools.ts
@@ -54,7 +54,7 @@ export default class SelectTools extends ValidaDue {
                     return;
                 }
             });
-        });
+        }, { immediate: true });
     }
 
     /**
